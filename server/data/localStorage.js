0b5020c2c901b85fc95d550ba193338655ddecf8
MoranSin <nurixhbh@gmail.com>
2024-02-12 15:05:02 +0200

    server up , get all works

--- server/data/localStorage.js
+++ server/data/localStorage.js
@@ -5,8 +5,7 @@ const Path = require('path');
 module.exports = class localStorage extends EventEmitter {
     constructor(entity) {
         super();
-        // this.path = Path.join(path, 'localStorage.json');
-        this.path = Path.join(__dirname, `./${entity}.json`);
+        this.dir = Path.join(__dirname, `./${entity}.json`);
         this.on('update-storage', () => {
             fs.writeFile(this.dir, JSON.stringify(this.data),'utf8', (err) => {
                 if (err) throw err;
@@ -23,12 +22,13 @@ module.exports = class localStorage extends EventEmitter {
                 save: () => this.emit('update-storage')
             };
         }
+
         find (){
         return Promise.resolve(this.storage.data);
         }
 
         retrieve(id) {
-            return Promise.resolve(this.storage.data.find(item => item.id === id));
+            return Promise.resolve(this.storage.data.find(item => item.missionId === id));
         }
 
         create(data){
@@ -38,13 +38,13 @@ module.exports = class localStorage extends EventEmitter {
         }
 
         delete(id){
-            const result = this.storage.data.filter(item => item.id !== id);
+            const result = this.storage.data.filter(item => item.missionId !== id);
             this.storage.save(result);
             return Promise.resolve(result);
         }
 
         update(id,data){
-            const result = this.storage.map(item => item.id === id ? ({id,...data}): item);
+            const result = this.storage.map(item => item.missionId === id ? ({missionId,...data}): item);
             this.storage.save();
             return Promise.resolve(result);
         }
