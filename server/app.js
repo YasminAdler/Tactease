ecef75417f9176e5bdb04315e55b4e3e7b0bfb8b
MoranSin <nurixhbh@gmail.com>
2024-02-12 11:28:08 +0200

    KAN-1 progress

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const logger = require('morgan')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(logger('dev'))
 
 app.listen(port, () => console.log(`Express server is running on port ${port}`))
