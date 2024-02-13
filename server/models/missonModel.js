1758b0181c760de3cd7a2a55ae99a7ecd84cc973
MoranSin <nurixhbh@gmail.com>
2024-02-13 17:51:30 +0200

    mission model done

--- server/models/missonModel.js
+++ server/models/missonModel.js
@@ -2,9 +2,10 @@ const { Schema, model } = require('mongoose')
 const MissionSchema = new Schema({
   missionId: { type: Number },
   missionType: { type: String },
-  location: { type: String },
-  deathCount: { type: Number },
-  damage: { type: String }
+  startDate: { type: Date },
+  endDate: { type: Date },
+  soldierCount: { type: Number },
+  soldiersOnMission: { type: Array }
 }, { collection: 'missions' })
 
 module.exports = model('Mission', MissionSchema)
