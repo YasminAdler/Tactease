5f2056b170dc804a41ba623eb62579a2b5447ecc
Adi Levi <adile0194@gmail.com>
2024-02-14 12:33:07 +0200

    KAN-1
    matches the mongoStorage

--- server/repositories/request.repository.js
+++ server/repositories/request.repository.js
@@ -6,13 +6,13 @@ const mongoStorage = new MongoStorage('reports');
 
 const findRequests = () => mongoStorage.find({});
 
-const retrieveRequest = (id) => mongoStorage.retrieve({ _id: id });
+const retrieveRequest = (id) => mongoStorage.retrieve(id);
 
 const createRequest = (report) => mongoStorage.create(report);
 
-const updateRequest = (id, report) => mongoStorage.update({ _id: id }, report);
+const updateRequest = (id, report) => mongoStorage.update(id, report);
 
-const deleteRequest = (report) => mongoStorage.delete({ _id: report });
+const deleteRequest = (report) => mongoStorage.delete(report);
 
 module.exports = {
     findRequests, retrieveRequest, createRequest, updateRequest, deleteRequest,
