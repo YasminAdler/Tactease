df8b0001f1f7cdfb441ace1d1edc01c8b93e0a32
Adi Levi <adile0194@gmail.com>
2024-02-20 20:46:19 +0200

    trying to make a algorithm middleware

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 require('express-async-errors');
 const express = require('express');
 const logger = require('morgan');
@@ -7,12 +8,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { missionsRouter } = require('./routers/missionRouter');
 const { soldierRouter } = require('./routers/soldierRouter');
-const { requestsRouter } = require('./routers/requestRouter');
+// const { requestsRouter } = require('./routers/requestRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
-app.use('/missions', missionsRouter);   
+app.use('/missions', missionsRouter);
 app.use('/soldier', soldierRouter);
 
 app.use(errorHandler);
