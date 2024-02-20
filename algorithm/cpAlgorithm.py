b5784f4b4ab72fee104ccc4646a948dfb1e2d99a
YasminAdler <adleryasmin@gmail.com>
2024-02-20 19:06:21 +0200

    cpAlgorithmFinite

--- algorithm/cpAlgorithm.py
+++ algorithm/cpAlgorithm.py
@@ -3,6 +3,7 @@ from ortools.sat.python import cp_model
 import json
 from datetime import datetime, timedelta
 from classes.soldier import Soldier
+from collections import defaultdict
 from functions import getMissions, getSoldiers, datetime_to_hours, hours_to_datetime
 
 MIN_REST_HOURS = 6  # Minimal resting time in hours
@@ -49,16 +50,11 @@ for soldier in soldiers:
             f'soldier_{soldierId_key}mission{missionId_key}'
         )
 
-
-# Constraint: Each mission must be assigned at least one soldier and by required soldiers number for the mission
-# for missionId_key in mission_intervals.keys():
-#     required_soldiers = next((mission.soldierCount for mission in missions if str(mission.missionId) == missionId_key), None)
-#     if required_soldiers is not None:
-#         model.Add(sum(soldier_mission_vars[(str(soldier.personalNumber), missionId_key)] for soldier in soldiers) >= required_soldiers)
-
 # try: constraint: fair durations:
+
 # Constraint: Balance mission hours among soldiers as evenly as possible
-# Calculate the total mission hours for each soldier
+#             Calculate the total mission hours for each soldier
+
 soldier_total_hours = {str(soldier.personalNumber): model.NewIntVar(0, 24, f"total_hours_{str(soldier.personalNumber)}") for soldier in soldiers}
 mission_durations = {}
 for mission in missions:
@@ -68,13 +64,6 @@ for mission in missions:
     missionId_key = str(mission.missionId)
     mission_intervals[missionId_key] = model.NewIntervalVar(start_hours, duration_hours, end_hours, f'mission_interval_{missionId_key}')
     mission_durations[missionId_key] = duration_hours
-
-# Create a BoolVar for each soldier-mission pair
-soldier_mission_vars = {}
-for soldier in soldiers:
-    for missionId_key in mission_intervals.keys():
-        soldierId_key = str(soldier.personalNumber)
-        soldier_mission_vars[(soldierId_key, missionId_key)] = model.NewBoolVar(f'soldier_{soldierId_key}mission{missionId_key}')
 
 # Calculate the total mission hours for each soldier
 soldier_total_hours = {str(soldier.personalNumber): model.NewIntVar(0, 24, f"total_hours_{str(soldier.personalNumber)}") for soldier in soldiers}
@@ -112,7 +101,6 @@ model.Minimize(max_hours_var - min_hours_var + (max_mission_count - min_mission_
 
 
 
-
 # end of constraint fair durations
 
 def dis_en_able_constraints(flag, action):
@@ -132,14 +120,8 @@ for missionId_key, interval_var in mission_intervals.items():
 def missions_overlap(mission1_id, mission2_id):
     start1, end1 = mission_intervals[mission1_id].StartExpr(), mission_intervals[mission1_id].EndExpr()
     start2, end2 = mission_intervals[mission2_id].StartExpr(), mission_intervals[mission2_id].EndExpr()
-    return (start1 < end2) and (start2 < end1)
+    return (((start1 < end2) and (start2 < end1)) or (start1 == end2) or (start2 == end1))
 
-# Constraint: a soldier must have a 6 hour break between each mission
-
-# def soldier_break(mission1_id, mission2_id):
-#     end1 = mission_intervals[mission1_id].EndExpr()
-#     start2 = mission_intervals[mission2_id].StartExpr()
-#     return (start2 - end1 < MIN_REST_HOURS)
 
 soldier_assigned_vars = {}
 for soldier in soldiers:
@@ -147,18 +129,50 @@ for soldier in soldiers:
     # This variable is 1 if the soldier is assigned to any mission, 0 otherwise
     soldier_assigned_vars[soldier_id] = model.NewBoolVar(f'soldier_assigned_{soldier_id}')
 
-
+##########################
 # constraint : Soldiers should be assigned equally
-for soldier_id in soldier_assigned_vars:
-    # Attempt to ensure every soldier has at least one mission assigned
-    model.Add(sum(soldier_mission_vars[(soldier_id, missionId_key)] for missionId_key in mission_intervals) >= 1)
 
-missions_assigned_to_soldier = [soldier_mission_vars[(soldier_id, missionId_key)] for missionId_key in mission_intervals]
-model.AddMaxEquality(soldier_assigned_vars[soldier_id], missions_assigned_to_soldier)
+total_mission_hours = sum(mission_durations.values())
+missions_by_date = defaultdict(list)
+for mission in missions:
+    # Convert start datetime to date (YYYY-MM-DD) for grouping
+    start_date = mission.startDate.date()
+    missions_by_date[start_date].append(mission)
 
+total_hours_per_day = {}
+avarage_mission_hours_for_soldier = {}
+for date, missions_on_date in missions_by_date.items():
+    total_hours = 0
+    for mission in missions_on_date:
+        start_hours = datetime_to_hours(mission.startDate)
+        end_hours = datetime_to_hours(mission.endDate)
+        duration_hours = end_hours - start_hours
+        total_hours += duration_hours
+    total_hours_per_day[date] = total_hours
+    avarage_mission_hours_for_soldier[date] = total_hours_per_day[date]/len(soldiers)
+print(avarage_mission_hours_for_soldier)
 
-# try: constraint: fair durations:
+for soldier in soldiers:
+    for date, avg_hours in avarage_mission_hours_for_soldier.items():
+        # Calculate the upper limit as 130% of the average hours
+        upper_limit_hours = avg_hours * 1.3
+        
+        # Calculate the total hours assigned to the soldier for this date
+        soldier_hours_for_date = []
+        for mission in missions_by_date[date]:  # Assuming missions_by_date is available
+            mission_id = str(mission.missionId)
+            if (soldier.personalNumber, mission_id) in soldier_mission_vars:
+                duration_hours = datetime_to_hours(mission.endDate) - datetime_to_hours(mission.startDate)
+                assigned_var = soldier_mission_vars[(soldier.personalNumber, mission_id)]
+                soldier_hours_for_date.append(assigned_var * duration_hours)
+                
+        if soldier_hours_for_date:  # If there are missions assigned to the soldier on this date
+            total_hours_for_date = sum(soldier_hours_for_date)
+            # Add constraint for total hours to be at least the average and not exceed the upper limit
+            model.Add(total_hours_for_date >= avg_hours)
+            model.Add(total_hours_for_date <= upper_limit_hours)
 
+#####################################
 
 # end of constraint fair durations
 
@@ -173,11 +187,7 @@ for soldier in soldiers:
                         soldier_mission_vars[(soldier_id, mission1_id)].Not(),
                         soldier_mission_vars[(soldier_id, mission2_id)].Not()
                     ])
-                # if soldier_break(mission1_id, mission2_id):
-                #     model.AddBoolOr([
-                #         soldier_mission_vars[(soldier_id, mission1_id)].Not(),
-                #         soldier_mission_vars[(soldier_id, mission2_id)].Not()
-                #     ])
+
 
 solver = cp_model.CpSolver()
 status = solver.Solve(model)
@@ -218,3 +228,4 @@ else:
     print("Solver status:", status)
     print("Solver statistics:")
     print(solver.ResponseStats())
+ 
