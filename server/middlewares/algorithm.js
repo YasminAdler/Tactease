7668a5fe09004756f903f9f49fb4b98c32af0660
MoranSin <nurixhbh@gmail.com>
2024-02-22 22:45:18 +0200

    more changes for connection alg

--- server/middlewares/algorithm.js
+++ server/middlewares/algorithm.js
@@ -3,39 +3,54 @@ const { soldiersController } = require('../controllers/soldierController');
 const { missionsController } = require('../controllers/missionsController');
 const spawner = require('child_process').spawn;
 
-
 exports.algorithmController = {
   async executeAlgorithm(req, res, next) {
-    try{
-
+    try {
       const missionRes = await missionsController.addMission(req, res, next);
       if (!missionRes || missionRes.length === 0) throw new BadRequestError('algorithm: missionRes is empty');
 
       const soldiersRes = await soldiersController.getSoldiersByClassId(req, res, next, 40);
       if (!missionRes || missionRes.length === 0) throw new BadRequestError('algorithm: soldiersRes is empty');
 
-      missionsJSON = JSON.stringify(missionRes);
-      soldiersJSON = JSON.stringify(soldiersRes);
+      const missionsJSON = JSON.stringify(missionRes);
+      const soldiersJSON = JSON.stringify(soldiersRes);
 
-      const dataToPass = {
-        dataSend: [ missionsJSON, soldiersJSON ],
-        dataRecieve: {}
-       };
-      
-      const pythonProcess = spawner('python', ['algorithm/cpAlgorithm.py', JSON.stringify(dataToPass)]);
-      
+      // const dataToPass = {
+      //   dataSend: [missionsJSON, soldiersJSON],
+      //   dataRecieve: {},
+      // };
+
+      let retrievedData = '';
+
+      const pythonProcess = spawner('python3', ['-c', `import algorithm.cpAlgorithm; algorithm.cpAlgorithm.scheduleAlg(${missionsJSON}, ${soldiersJSON})`]);
+
       pythonProcess.stdout.on('data', (data) => {
-        const retreivedData = JSON.parse(data.toString());
-        console.log(retreivedData);
+        retrievedData += JSON.stringify(data);
+        if (!res.headersSent) { // Check if headers have already been sent
+          res.status(200).json(retrievedData);
+          console.log(retrievedData);
+        }
+        // res.status(200).json(retreivedData);
       });
-    }catch(error){
+
+      pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+        res.status(400).json(data);
+      });
+      // pythonProcess.stdout.pipe(process.stdout);
+      pythonProcess.on('close', (code) => {
+        if (code === 0) {
+          const parsedData = JSON.parse(retrievedData);
+          res.status(200).json(parsedData);
+        } else {
+          console.log(`child process exited with code ${code}`);
+        }
+      });
+    } catch (error) {
       next(error);
     }
-
+  },
 };
-
-
-
 
 // const options = {
 //   scriptPath: 'algorithm',
