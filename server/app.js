e5817de72bcf609c6f0fa9c8c6e143de09bc35d6
MoranSin <nurixhbh@gmail.com>
2024-03-04 21:26:09 +0200

    added cors

--- server/app.js
+++ server/app.js
@@ -2,14 +2,15 @@
 require('express-async-errors');
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const { missionsRouter } = require('./routers/missionRouter');
 const { soldierRouter } = require('./routers/soldierRouter');
-// const { requestsRouter } = require('./routers/requestRouter');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger('dev'));
