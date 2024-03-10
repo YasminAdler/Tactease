31b565937a5c86966ba4746ccc3f2fef96e2b6b4
Adi Levi <adile0194@gmail.com>
2024-03-10 15:11:26 +0200

    try  2

--- server/middlewares/algorithm.js
+++ server/middlewares/algorithm.js
@@ -35,7 +35,7 @@ exports.algorithmController = {
       const missionsJSON = JSON.stringify(missionArr);
       const soldiersJSON = JSON.stringify(soldierRes);
 
-      const pythonScriptPath = path.join(__dirname, '..', 'algorithm', 'cpAlgorithm.py');
+      const pythonScriptPath = path.join(__dirname, '..', '..', 'algorithm', 'cpAlgorithm.py');
       const pythonProcess = spawner('python', [pythonScriptPath, missionsJSON, soldiersJSON]);
 
       //      const pythonProcess = spawner('python', ['-c', `import algorithm.cpAlgorithm; algorithm.cpAlgorithm.scheduleAlg(${missionsJSON}, ${soldiersJSON})`]);
