1200bb4ca8341813f655424bae1f31730abaedc1
Adi Levi <adile0194@gmail.com>
2024-03-14 12:39:22 +0200

    trying to use updated functions

--- algorithm/functions.py
+++ algorithm/functions.py
@@ -1,77 +1,81 @@
 from datetime import datetime, timedelta
 from classes.mission import Mission
 from classes.soldier import Soldier
+from classes.request import Request, PersonalRequest, MedicalRequest
 
 def getMissions(missions_data):
     missions = []
-    try:
-        for inputMission in missions_data:
-            mission = Mission(
-                missionId=str(inputMission["_id"]),
-                missionType=inputMission["missionType"],
-                startDate= inputMission["startDate"],
-                endDate= inputMission["endDate"],
-                soldierCount=int(inputMission["soldierCount"]),
-                soldiersOnMission=inputMission["soldiersOnMission"]
-            )
-            missions.append(mission)
-        return missions
-    except Exception as e:
-        print("Error occurred:", e)
-
+    for mission_data in missions_data:
+        mission = Mission(
+            missionId=str(mission_data["_id"]),
+            missionType=mission_data["missionType"],
+            startDate=mission_data["startDate"],
+            endDate=mission_data["endDate"],
+            soldierCount=int(mission_data["soldierCount"]),
+            soldiersOnMission=mission_data.get("soldiersOnMission", [])
+        )
+        missions.append(mission)
+    return missions
 
 def getSoldiers(soldiers_data):
     soldiers = []
-    for soldierInput in soldiers_data:
+    for soldier_data in soldiers_data:
         soldier = Soldier(
-            personalNumber=int(soldierInput["personalNumber"]),  # Change soldier_data.personalNumber to soldier_data["personalNumber"]
-            fullName=str(soldierInput["fullName"]),  # Change soldier_data.fullName to soldier_data["fullName"]
-            classId=int(soldierInput["depClass"]["classId"]),  # Change soldier_data.depClass.classId to soldier_data["depClass"]["classId"]
-            className=str(soldierInput["depClass"]["className"]),  # Change soldier_data.depClass.className to soldier_data["depClass"]["className"]
-            pakal=str(soldierInput["pakal"]),  # Change soldier_data.pakal to soldier_data["pakal"]
-        )
+            personalNumber=int(soldier_data["personalNumber"]),
+            fullName=str(soldier_data["fullName"]),
+            classId=int(soldier_data['depClass']['classId']),
+            className=str(soldier_data['depClass']["className"]),
+            pakal=str(soldier_data["pakal"]),
+            # Assuming requestList contains mission assignment info; adapt as necessary
+            )
         soldiers.append(soldier)
     return soldiers
 
+def getRequests(requests_data):
+    requests = []
+    for request_data in requests_data:
+        # Check if it's a MedicalRequest or PersonalRequest by checking for 'file' and 'fileName'
+        if 'file' in request_data and 'fileName' in request_data:
+            request = MedicalRequest(
+                requestType=request_data["requestType"],
+                daysOffType=request_data["daysOffType"],
+                start_date=request_data["start_date"],
+                end_date=request_data["end_date"],
+                file=request_data["file"],
+                fileName=request_data["fileName"]
+            )
+        elif 'note' in request_data:
+            request = PersonalRequest(
+                requestType=request_data["requestType"],
+                daysOffType=request_data["daysOffType"],
+                start_date=request_data["start_date"],
+                end_date=request_data["end_date"],
+                note=request_data["note"]
+            )
+        else:
+            # Handle generic requests or log an error/warning
+            request = Request(
+                requestType=request_data["requestType"],
+                daysOffType=request_data["daysOffType"],
+                start_date=request_data["start_date"],
+                end_date=request_data["end_date"]
+            )
+        requests.append(request)
+    return requests
 
-
-
-
-
-
-# def getMissions(missions_data):
-#     missions = []
-#     try:
-#         with open('algorithm/mission_data.txt', 'a') as f:
-#             f.write(mission_data)
-#         for mission in missions_data:
-#             mission = Mission(
-#                 missionId=str(mission_data._id),
-#                 missionType=mission_data.missionType,
-#                 startDate= mission_data.startDate,
-#                 endDate= mission_data.startEnd,
-#                 soldierCount=int(mission_data.soldierCount),
-#                 soldiersOnMission=mission_data.soldiersOnMission
-#             )
-#             missions.append(mission)
-#     except Exception as e:
-#         print("Error occurred:", e)
-#     return missions
-
-
-# def getSoldiers(soldiers_data):
-#     soldiers = []
-#     for soldier_data in soldiers_data:
-#         soldier = Soldier(
-#             personalNumber=int(soldier_data.personalNumber),
-#             fullName=str(soldier_data.fullName),
-#             classId=int(soldier_data.depClass.classId),
-#             className=str(soldier_data.depClass.className),
-#             pakal=str(soldier_data.pakal),
-#             )
-#         soldiers.append(soldier)
-#     return soldiers
-
+def parseRequests(requestList):
+    requests = []
+    for request_data in requestList:
+        request = Request(
+            requestType=request_data["requestType"],
+            daysOffType=request_data["daysOffType"],
+            start_date=request_data["startDate"],
+            end_date=request_data["endDate"],
+            note=request_data.get("note"),
+            fileName=request_data.get("fileName")
+        )
+        requests.append(request)
+    return requests
 
 def datetime_to_hours(datetime_input):
     """Function to convert datetime strings to a consistent unit (e.g., hours)"""
